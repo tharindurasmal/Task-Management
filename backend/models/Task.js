1a3ee62d5d37b14777a70b1e6c4dc01c1499de77
tharindurasmal <tharindurasmal1@gmail.com>
2026-09-04 21:50:27 +0530

    task model

---
+++ backend/models/Task.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      default: '',
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ['todo', 'doing', 'done'],
+      default: 'todo',
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      default: null,
+    },
+  },
+  { timestamps: true } 
+);
+
+module.exports = mongoose.model('Task', taskSchema);
